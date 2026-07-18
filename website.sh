d9134364f591e1243e54167579452e9ebd54283d
renspringlea <ren.springlea@animalask.org>
2026-07-19 06:17:47 +0930

    Update

--- website.sh
+++ website.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-cd ~/shefootball
+cd ~/Documents/Websites/shefootball
 #bundle lock --add-platform x86_64-linux
 #bundle exec jekyll build --verbose
 #bundle exec jekyll serve
 #git config pull.rebase false
-git pull
+Rscript analysis_files/acl.R
+# git pull
 git add .
 git commit -m "Update"
 git push
